89f6fe4086aa23e0ea1e94d6325edbb45b164ae0
Krzysztof Zbicinski <fistasheq+github@gmail.com>
2017-10-11 18:58:28 +0200

    Add trailing dot

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function unsplashTag(args, content) {
     var id = args[1];
     var author = args[2];
 
-    return '<' + tag + ' class="unsplash-credit">Photo by <a href="https://unsplash.com/photos/' + id + '" target="_blank">' + author + '</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a></' + tag + '>';
+    return '<' + tag + ' class="unsplash-credit">Photo by <a href="https://unsplash.com/photos/' + id + '" target="_blank">' + author + '</a> on <a href="https://unsplash.com/" target="_blank">Unsplash</a>.</' + tag + '>';
 };
 
 hexo.extend.tag.register('unsplash', unsplashTag);
